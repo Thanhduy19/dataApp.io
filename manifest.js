9bbf6c42ebb4d3abf23d909eadba1911d2a885bc
Thanhduy19 <66198277+Thanhduy19@users.noreply.github.com>
2026-06-17 21:52:59 +0700

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,10 +1,9 @@
 {
-  "version": 3,
+  "version": 1,
   "files": {
-    "boot.html":    "sha256-…",
-    "wasm_exec.js": "sha256-…",
-    "libsodium.js": "sha256-…",
-    "sodium.js":    "sha256-…",
-    "fu.wasm":      "sha256-…"
+    "boot.html": "sha256-3da657e3dd70866f6cf9a580d3ba1e7122bb64ffc63ab9a4225e62c7a2bbbbb3",
+    "wasm_exec.js": "sha256-ec45eef6a873fa4083143ae60815fe8d22c9cd2add541c4c0a3200dd79b3b90c",
+    "libsodium.js": "sha256-ff3c1112341391785c9ab3e85313975a3d7614e63b3d3efac3ca6d9867a876d6",
+    "sodium.js": "sha256-1261af2abd7cdee94e8665b3dfd4e57ab06c064e0e983690f6085e7960a7222e"
   }
 }
