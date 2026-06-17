e98a1aaa350ab99d0afd6e0b8a6f5b4634a6c223
Thanhduy19 <66198277+Thanhduy19@users.noreply.github.com>
2026-06-17 21:39:37 +0700

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,10 @@
+{
+  "version": 3,
+  "files": {
+    "boot.html":    "sha256-…",
+    "wasm_exec.js": "sha256-…",
+    "libsodium.js": "sha256-…",
+    "sodium.js":    "sha256-…",
+    "fu.wasm":      "sha256-…"
+  }
+}
